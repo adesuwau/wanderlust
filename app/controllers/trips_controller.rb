bb7ae10aec8b6c87105c229d4b5092a3f28887dc
adesuwau <missadeugbogbo@gmail.com>
2014-10-25 14:21:32 -0400

    Fixes users/trips view, adds datamaps.js

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -4,7 +4,7 @@ class TripsController < ApplicationController
   # GET /trips
   # GET /trips.json
   def index
-    @trips = Trip.all
+  @user = User.find_by(id: params[:user_id])
   end
 
   # GET /trips/1
